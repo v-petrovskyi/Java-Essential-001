811dfc819760f22c4495ca73c79e9554c6368971
Volodymyr <bugsbunny1163@gmail.com>
2022-05-01 19:39:57 +0300

    task2 done

--- src/task2/Rectangle.java
+++ src/task2/Rectangle.java
@@ -1,4 +1,26 @@
 package task2;
 
+import java.util.Scanner;
+
 public class Rectangle {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введіть довжини сторін");
+        double side1 = scanner.nextDouble();
+        double side2 = scanner.nextDouble();
+        System.out.println("Area = " + rectangle.areaCalculator(side1, side2));
+        System.out.println("Perimeter = " + rectangle.perimeterCalculator(side1, side2));
+
+    }
+
+    double areaCalculator(double side1, double side2) {
+        return side1 * side2;
+    }
+
+    double perimeterCalculator(double side1, double side2) {
+        return 2 * (side1 + side2);
+    }
+
+
 }
